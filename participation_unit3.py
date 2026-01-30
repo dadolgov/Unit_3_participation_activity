43d53ce0250e53d5dd15bb4cced70d20f847fb1a
Dmitrii Dolgov <ddolgov1@student.cscc.edu>
2026-01-30 12:55:33 -0500

    Added .pop() to avoid repetitions

--- participation_unit3.py
+++ participation_unit3.py
@@ -9,16 +9,22 @@ import random
 dragon_names=["yse", "jinim","fraghinth","zathyd","ayndran","edyr",
              "grimmyn","paidacro","neikinoig","agocrem"]
 dragon_titles=["the swift","longtail","braveheart","lord of the yellow",
-              "the young one","the protective","the victorious","the deathlord"
+              "the young one","the protective","the victorious","the deathlord",
               "the devourer", "the despoiler"]
+
 #safeguard in case of different list lengths
 if len(dragon_names)<len(dragon_titles):
     max_index=len(dragon_names)-1
 else:
     max_index=len(dragon_titles)-1
+
 #output_header
 print("Here are the names of your dragons:")
+
 #iterating and making names
 for index in range(max_index+1):
-    full_name=f"{dragon_names[random.randint(0,max_index)].title()} {dragon_titles[random.randint(0,max_index)].title()}."
+    chosen_name=dragon_names.pop(random.randint(0,max_index)).title()
+    chosen_title=dragon_titles.pop(random.randint(0,max_index)).title()
+    full_name=f"{chosen_name} {chosen_title}"
     print(full_name)
+    max_index-=1
