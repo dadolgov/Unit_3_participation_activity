4cecd79dc22927be95029d74d57764c26f132ec0
Dmitrii Dolgov <ddolgov1@student.cscc.edu>
2026-01-30 12:15:02 -0500

    first commit. Initialized the lists

---
+++ participation_unit3.py
@@ -0,0 +1,15 @@
+"""
+Dragon name generator
+Author: Dmitrii Dolgov
+Purpose: generates random dragon names from 2 lists
+Date: 1/30/2026
+"""
+import random
+
+dragon_names=["yse", "jinim","fraghinth","zathyd","ayndran","edyr",
+             "grimmyn","paidacro","neikinoig","agocrem"]
+dragon_titles=["the swift","longtail","braveheart","lord of the yellow",
+              "the young one","the protective","the victorious","the deathlord"
+              "the devourer", "the despoiler"]
+
+
